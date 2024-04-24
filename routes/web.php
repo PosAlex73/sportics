f15a00a3e83f653468370ea0b77781a1eb079f8a
aposnov <a.posnov@fairtech.ru>
2024-04-24 13:03:08 +0400

    проверка апи

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\IndexController::class, 'index']);
+
+require __DIR__.'/auth.php';
